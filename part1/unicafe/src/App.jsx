0c987cd7db588fe51880c1f4b4a33091e82b8a14
fkurusu7 <f.douzez@gmail.com>
2024-07-29 00:52:31 -0600

    finish part1 sections 1.12 1.13 1.14

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,16 @@
 import { useState } from "react";
 
+const ANECDOTES = [
+  "If it hurts, do it more often.",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+  "The only way to go fast, is to go well.",
+];
+
 const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
@@ -47,6 +58,55 @@ const Statistics = ({ good, neutral, bad, all }) => {
   );
 };
 
+const Anedocte = () => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(() => {
+    const anecdotesObj = {};
+    ANECDOTES.forEach((_, index) => {
+      anecdotesObj[index] = 0;
+    });
+    return anecdotesObj;
+  });
+
+  console.log("VOTES: ", votes);
+  const handleSelected = () => {
+    const randomNum = Math.random() * ANECDOTES.length;
+    const index = Math.floor(randomNum);
+    console.log(randomNum, index);
+    setSelected(index);
+  };
+
+  const handleVote = () => {
+    setVotes({ ...votes, [selected]: votes[selected] + 1 });
+  };
+
+  const showAnecdoteWithMostVotes = () => {
+    const maxEntry = Number(
+      Object.entries(votes).reduce(
+        (maxValue, current) => {
+          console.log(current, maxValue);
+          return current[1] > maxValue[1] ? current : maxValue;
+        },
+        [null, -Infinity]
+      )[0]
+    );
+    console.log(maxEntry);
+    return ANECDOTES[maxEntry];
+  };
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <p>{ANECDOTES[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button text="vote" onClick={handleVote} />
+      <Button text="next anecdote" onClick={handleSelected} />
+      <h2>Anecdote with most votes</h2>
+      <p>{showAnecdoteWithMostVotes()}</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -68,14 +128,15 @@ const App = () => {
     setBad(bad + 1);
     setAll(1 + handleAll());
   };
+
   return (
     <div>
       <h1>Give Feedback</h1>
       <Button text="good" onClick={handleGood} />
       <Button text="neutral" onClick={handleNeutral} />
       <Button text="bad" onClick={handleBad} />
-
       <Statistics good={good} neutral={neutral} bad={bad} all={all} />
+      <Anedocte />
     </div>
   );
 };
