d9306ab1d18607b241e052b1844e4c4932d9db16
fkurusu7 <f.douzez@gmail.com>
2024-07-28 23:38:03 -0600

    finish part1 section 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,26 +9,43 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+
+  const handleAll = () => good + neutral + bad;
 
   const handleGood = () => {
     setGood(good + 1);
+    setAll(1 + handleAll());
   };
   const handleNeutral = () => {
     setNeutral(neutral + 1);
+    setAll(1 + handleAll());
   };
   const handleBad = () => {
     setBad(bad + 1);
+    setAll(1 + handleAll());
   };
+
+  const GOOD_C = 1;
+  const NEUTRAL_C = 0;
+  const BAD_C = -1;
+  console.log("sum by cat:", good * GOOD_C + neutral * NEUTRAL_C + bad * BAD_C);
+  const average = (good * GOOD_C + neutral * NEUTRAL_C + bad * BAD_C) / all;
+  const positive = (good / all) * 100;
   return (
     <div>
       <h1>Give Feedback</h1>
       <Button text="good" onClick={handleGood} />
       <Button text="neutral" onClick={handleNeutral} />
       <Button text="bad" onClick={handleBad} />
+
       <h2>Statistics</h2>
-      <p>good - {good}</p>
-      <p>neutral - {neutral}</p>
-      <p>bad - {bad}</p>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   );
 };
