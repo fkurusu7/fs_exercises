55a2551e1f2e2af01247fef4e6b3e174dbba3efd
fkurusu7 <f.douzez@gmail.com>
2024-08-07 01:34:06 -0600

    finish part3 a 3.7 3.8*

---
+++ part3/ex_31_38/index.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+app.use(express.json());
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+morgan.token("type", function (req, res) {
+  return JSON.stringify(req.body);
+});
+morgan.format(
+  "kurusu",
+  ":method :url :status :response-time ms - :res[content-length] :type"
+);
+app.use(morgan("kurusu"));
+// POST /api/persons 200 2.818 ms - 53 {"name":"Fer Cuva","number":"01-654321-123"}
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, Express!</h1>");
+});
+
+app.get("/info", (req, res) => {
+  const totalPersons = `<p>Phonebook has info for ${persons.length} people</p>
+  `;
+
+  const fullDate = new Date(8.64e15).toString();
+  const stringDate = `<p>${fullDate}</p>`;
+
+  res.send(`<h1>Hello, FullStack!</h1>${totalPersons}${stringDate}`);
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).json({ error: `no person found with id ${id}` });
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
+const generateId = () => {
+  const maxId =
+    persons.length > 0
+      ? Math.max(...persons.map((person) => Number(person.id)))
+      : 0;
+
+  return String(maxId + 1);
+};
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  if (!body.name) {
+    return res.status(400).json({ error: "name missing" });
+  } else if (!body.number) {
+    return res.status(400).json({ error: "number missing" });
+  }
+
+  const nameExists = persons.some((person) => person.name === body.name);
+  if (nameExists) return res.status(409).json({ error: "name must be unique" });
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
