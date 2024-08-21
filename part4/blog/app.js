184c64141817b5d6a766267a197abd07e0189322
fkurusu7 <coding.fcv@gmail.com>
2024-08-20 20:41:17 -0600

    finish part4 d 4.18

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const postsRouter = require("./controllers/posts");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
@@ -40,6 +41,7 @@ app.use(morgan("mFormat"));
 // ROUTES
 app.use("/api/blog/posts", postsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
