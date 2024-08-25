d1a3875ef0add9021291f612b22d6f8a86ea7f72
fkurusu7 <coding.fcv@gmail.com>
2024-08-24 23:50:30 -0600

    style posts list

--- part5/ui_blog/src/components/Post.jsx
+++ part5/ui_blog/src/components/Post.jsx
@@ -1,7 +1,10 @@
-const Post = ({ post }) => (
-  <li>
-    {post.title} - {post.author}
-  </li>
-);
+function Post({ post }) {
+  return (
+    <li>
+      <span className="title">{post.title}</span>
+      by <span className="author">{post.author}</span>
+    </li>
+  );
+}
 
 export default Post;
