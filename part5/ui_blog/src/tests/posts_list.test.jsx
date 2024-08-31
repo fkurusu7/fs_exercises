ad6235fa88183e613ac5dedc6967f4cc5f9837cc
fkurusu7 <coding.fcv@gmail.com>
2024-08-31 00:29:16 -0600

    finish part 5 c 5.14

--- part5/ui_blog/src/tests/posts_list.test.jsx
+++ part5/ui_blog/src/tests/posts_list.test.jsx
@@ -27,3 +27,33 @@ test("should check a post renders the title and author, but do not render its ur
   const likesElement = container.querySelector(".likes");
   expect(likesElement).toBeNull();
 });
+
+test("should show url and likes fields when the view button is clicked", async () => {
+  const post = {
+    title: "Testing the title",
+    author: "Kurusu",
+    url: "url://test.com",
+    likes: 3,
+    user: { username: "Kurusu" },
+  };
+
+  const { container } = render(
+    <Post
+      post={post}
+      setPosts={() => console.log()}
+      handleMessage={() => console.log()}
+    />
+  );
+
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+
+  // Check URL and Likes are not visible initially
+  expect(screen.queryByText("url://test.com")).not.toBeInTheDocument();
+  expect(screen.queryByText("Likes: 3")).not.toBeInTheDocument();
+
+  // Check URL and Likes are are now visible
+  await user.click(button);
+  expect(screen.queryByText("url://test.com")).toBeDefined();
+  expect(container.querySelector(".likes")).toBeDefined();
+});
